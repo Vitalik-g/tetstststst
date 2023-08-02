b4dedbbac5a2874e9cbf52ed3f178a22a0f43593
volkkto <volkkto@gmail.com>
2023-08-02 09:03:57 +0300

    v2

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
+var http = require('http');
+var options = {
+  host: 'test-server',
+  path: '/'
+};
+
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  getter(res)
 })
-
+function getter(res){
+  var req = http.get(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+  
+    // Buffer the body entirely for processing as a whole.
+    var bodyChunks = [];
+    res.on('data', function(chunk) {
+      // You can process streamed parts here...
+      bodyChunks.push(chunk);
+    }).on('end', function() {
+      var body = Buffer.concat(bodyChunks);
+      res.send(body)
+      // ...and/or process the entire body here.
+    })
+  });
+}
 app.listen(3000)
